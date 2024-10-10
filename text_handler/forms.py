ed9967d79bc456eab83292ac260ceceac95e7b12
Pavel Drovnin <pavel98rus@gmail.com>
2024-10-10 20:59:00 +0300

    forms

---
+++ text_handler/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from text_handler.models import Document
+
+
+ALLOWED_SUFFIX = ['txt',]
+
+
+class DocumentForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Document
+        fields = ('file',)
+        help_text = {'file': 'Выберите текстовый документ', }
+        label = {'file': 'Документ', }
+
+        def clean_text(self):
+            file = self.cleaned_data['file']
+            if not file:
+                raise forms.forms.ValidationError('Необходимо выбрать файл')
+            # TODO проверку расширения файла
+            return file
